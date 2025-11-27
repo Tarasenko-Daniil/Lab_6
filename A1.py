cb2c5649acd52fb6d58291e4ba166863ab56a180
Daniil Tarasenko <tarasenko_dd_25@student.grsu.by>
2025-11-27 19:28:14 +0300

    Update A1.py
    добавил функцию get_stats(users, workouts) для расчета общей статистики, а также изменил функции  load_users_data() и oad_workouts_data() сделав так чтобы информация об пользователе или о тренировке хранилась как словарь, который в свою очередь с остальными словарями хранятся в списке

--- A1.py
+++ A1.py
@@ -3,29 +3,59 @@ import xml.etree.ElementTree as et
 def load_users_data(filename):
     tree = et.parse(filename)
     root = tree.getroot()
-
+    users = []
     for user in root.findall("user"):
-        user_id = user.find("user_id").text
-        name = user.find('name').text
-        age = user.find('age').text
-        weight = user.find('weight').text
-        fitness_level = user.find('fitness_level').text
-
-        print(f"id: {user_id}, Имя: {name}, Возраст: {age}, Вес: {weight}, Уровень подготовки: {fitness_level}")
+        users.append({
+            'user_id': user.find("user_id").text,
+            'name': user.find('name').text,
+            'age': int(user.find('age').text),
+            'weight': float(user.find('weight').text),
+            'fitness_level': user.find('fitness_level').text})
+    return users
 
 def load_workouts_data(filename):
     tree = et.parse(filename)
     root = tree.getroot()
-
+    workouts = []
     for workout in root.findall("workout"):
-        workout_id = workout.find("workout_id").text
-        user_id = workout.find("user_id").text
-        date = workout.find('date').text
-        type = workout.find('type').text
-        duration = workout.find('duration').text
-        distance = workout.find('distance').text
-        calories = workout.find('calories').text
-        avg_heart_rate = workout.find('avg_heart_rate').text
-        intensity = workout.find('intensity').text
+        workouts.append({
+            'workout_id': workout.find("workout_id").text,
+            'user_id': workout.find("user_id").text,
+            'date': workout.find('date').text,
+            'type': workout.find('type').text,
+            'duration': float(workout.find('duration').text),
+            'distance': float(workout.find('distance').text),
+            'calories': float(workout.find('calories').text),
+            'avg_heart_rate': float(workout.find('avg_heart_rate').text),
+            'intensity': workout.find('intensity').text})
+    return workouts
+
+def get_stats(users, workouts):
+    print("ОБЩАЯ СТАТИСТИКА")
+    print(30*"-")
+    print(f"Всего тренировок: {len(workouts)}")
+    print(f"Всего пользователей: {len(users)}")
+
+    sum_calor = 0
+    for workout in workouts:
+        sum_calor += workout['calories']
+    print(f"Сожжено калорий: {int(sum_calor)}")
+
+    sum_duration = 0
+    for workout in workouts:
+        sum_duration += workout['duration']
+    h = int(sum_duration // 60)
+    m = int(sum_duration % 60)
+    print(f"Общее время: {h}.{m}")
+    
+    sum_distance = 0
+    for workout in workouts:
+        sum_distance += workout['distance']
+    print(f"Пройдено дистанции: {sum_distance:.1f}")
+
+users = load_users_data('users.xml')
+workouts = load_workouts_data('workouts.xml')
+get_stats(users, workouts)
+
 
 
